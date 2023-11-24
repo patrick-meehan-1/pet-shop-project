7398abeb58bc4b769b9de31d8092b8db0a90a7db
Patrick Meehan <pjmeehanpj33@gmail.com>
2023-11-24 11:44:28 +0000

    Added templates, views and urls for the categories, products etc. Also setup the home page with a logo and banner. Working on context processor next.

---
+++ shop/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+
+# Create your views here.
+def prod_list(request, category_id=None):
+    category = None
+    products = Product.objects.filter(available=True)
+    if category_id:
+        category = get_object_or_404(Category, id=category_id)
+        products = Product.objects.filter(category=category, available=True)
+    return render(request, 'shop/category.html', {'category': category, 'prods': products})
+
+# def product_detail(request, category_id, product_id):
+#     product = get_object_or_404(Product, category_id=category_id, id=product_id)
+#     return render(request, 'shop/product.html', {'product': product})
