7b0fd94a2b0fe3768d0aaafd7611a4d348951eb4
Patrick Meehan <pjmeehanpj33@gmail.com>
2023-12-01 11:45:37 +0000

    had to re-install all packages..not sure why. Also tried replacing the password change template, but not working

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=True)
-    f_animal = models.CharField(max_length=250, unique=True)
+    f_animal = models.CharField(max_length=250, blank=True)
